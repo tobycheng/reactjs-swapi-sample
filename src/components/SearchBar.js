6e98e8a879e0ada42c9ec37012c9da423d5ae81d
tobycheng <tobycfcheng@gmail.com>
2023-08-31 15:39:46 -0700

    Add page title

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -40,7 +40,10 @@ function SearchBar() {
   return (
     <>
       <div></div>
-      <div>
+      <div><h1 style={{'text-align': 'center'}}>React + Star Wars API</h1></div>
+      <div></div>
+      <div></div>
+      <div style={{'text-align': 'center'}}>
         <select value={dropdownValue} onChange={handleDropdownChange}>
           {options.map((option)=>(
             <option value={option.value}>{option.text}</option>
