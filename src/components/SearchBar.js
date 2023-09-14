4f82e999bcef43b0450e134fa2158e79aea3cf7e
tobycheng <tobycfcheng@gmail.com>
2023-09-13 18:10:59 -0700

    Update dropdown to use MUI Select
    also update to MUI Search Button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,93 @@
 import sendRequest from "./SendRequest";
 import { useDropdownContext } from "../context/DropdownContext";
 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const selectTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFE81F',
+      dark: '#b2a215',
+    },
+  },
+  components: {
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: '#FFE81F'
+        }
+      }
+    },
+    MuiBaseInput: {
+      styleOverrides: {
+        root: {
+          border: '1px #FFE81F'
+        }
+      }
+    },
+    MuiSelect: {
+      styleOverrides: {
+        root: {
+          color: '#FFE81F',
+          border: '1px #FFE81F',
+        },
+        select: {
+          ":hover": {
+            backgroundColor: "#b2a215",
+            // border: '2px solid #b2a215',
+          },
+        },
+      },
+    },
+    MuiList: {
+      styleOverrides:{
+        root: {
+          color: '#FFE81F',
+          backgroundColor: "black",
+        }
+      }
+    },
+  }
+})
+
+// const selectTheme = createTheme({
+//   root: {
+//     width: 200,
+//     "& .MuiOutlinedInput-input": {
+//       color: "green"
+//     },
+//     "& .MuiInputLabel-root": {
+//       color: "green"
+//     },
+//     "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+//       borderColor: "green"
+//     },
+//     "&:hover .MuiOutlinedInput-input": {
+//       color: "red"
+//     },
+//     "&:hover .MuiInputLabel-root": {
+//       color: "red"
+//     },
+//     "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+//       borderColor: "red"
+//     },
+//     "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
+//       color: "purple"
+//     },
+//     "& .MuiInputLabel-root.Mui-focused": {
+//       color: "purple"
+//     },
+//     "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+//       borderColor: "purple"
+//     }
+//   }
+// });
 
 const LoadBar = () => {
   const {loading} = useDropdownContext();
@@ -40,19 +127,34 @@ function SearchBar() {
   return (
     <>
       <div></div>
-      <div><h1 style={{'textAlign': 'center'}}>React + Star Wars API</h1></div>
+      <div><h1 style={{'textAlign': 'center'}}>ReactJS + Star Wars API</h1></div>
       <div></div>
+
       <div></div>
       <div style={{'textAlign': 'center'}}>
-        <select value={dropdownValue} onChange={handleDropdownChange}>
-          {options.map((option)=>(
-            <option value={option.value}>{option.text}</option>
-          ))}
-        </select>
 
-        <button onClick={()=>handleSearch(dropdownValue)}>Search</button>
+        <ThemeProvider theme={selectTheme}>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Category</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={dropdownValue}
+                label="Category"
+                onChange={handleDropdownChange}
+              >
+                {options.map((option)=>(
+                  <MenuItem value={option.value}>{option.text}</MenuItem>
+                ))}
+              </Select>
+              <Button variant="contained" onClick={()=>handleSearch(dropdownValue)}>Search</Button>
+            </FormControl>
+          </Box>
+        </ThemeProvider>
       </div>
       <div></div>
+
       <LoadBar />
     </>
   )
