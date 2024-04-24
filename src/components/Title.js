37750f2a37fa18cc4aff03a47e4d532b91d4bde4
tobycheng <tobycfcheng@gmail.com>
2024-04-24 16:48:35 -0700

    create Title component

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box';
+
+function Title() {
+  return (
+    <Box textAlign="center">
+      <h1>Reactjs -  Star Wars api</h1>
+    </Box>
+  )
+}
+
+export default Title;
