23094a67b71db337f8bab3e46d176f2125fa3530
tobycheng <tobycfcheng@gmail.com>
2024-04-25 15:50:35 -0700

    update title font

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,8 +2,11 @@ import Box from '@mui/material/Box';
 
 function Title() {
   return (
-    <Box textAlign="center">
-      <h1>Reactjs -  Star Wars api</h1>
+    <Box
+    textAlign="center"
+    fontFamily='Star Wars, sans-serif'
+    >
+      <h1>Reactjs - Star Wars api</h1>
     </Box>
   )
 }
