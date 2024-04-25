c15584b1aa133b89b50f67ecfede62196d6a0a4a
tobycheng <tobycfcheng@gmail.com>
2024-04-25 15:53:20 -0700

    add MUI base theme for styling

---
+++ src/styles/baseTheme.js
@@ -0,0 +1,25 @@
+import { createTheme } from '@mui/material/styles';
+
+
+const baseTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#FFE81F',
+    },
+    text: {
+      primary: '#FFE81F',
+      secondary: '#FFE81F',
+    },
+    background: {
+      default: '#060607',
+      paper: '#060607',
+    },
+  },
+  typography: {
+    fontFamily: "Helvetica",
+  }
+
+})
+
+export default baseTheme;
